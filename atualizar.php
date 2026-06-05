e5f404b54c56f9782004f2308a82a89362e01eef
flow-znn <tiagorodrigues1455@gmail.com>
2026-06-05 18:08:21 -0300

    atualizar.php

--- atualizar.php
+++ atualizar.php
@@ -1,1 +1,34 @@
-# Crud-Login
+<?php
+include("conexao.php");
+
+
+if($_POST['acao'] == 'editar'){
+$id = $_POST['id'];
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+if(!empty($senha)) {
+    $sql = "UPDATE usuarios
+            SET nome = '$nome',
+                email = '$email',
+                senha = '$senha'
+            WHERE id = $id";
+} else {
+    $sql ="UPDATE usuarios
+            SET nome = '$nome',
+                email = '$email'
+            WHERE id = $id";
+}
+
+if($conn->query($sql) === TRUE) {
+    header("Location: dashboard.php?status=atualizado");
+    exit;
+} else {
+    header("Location: dashboard.php?status=error");
+    exit;
+}
+}
+
+
+?>
